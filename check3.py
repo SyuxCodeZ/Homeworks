30f9bd5e692d8beb24d7f79a94b61d7a6adc919e
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-30 15:04:25 +0600

    check

---
+++ check3.py
@@ -0,0 +1,18 @@
+# input element
+
+char = input("Enter a character: ")
+
+# conditional statements
+
+if char.isalpha():
+
+    print(f"'{char}' is an alphabet.")
+
+elif char.isalpha() == False:
+
+    print(f"{char} is not an alphabet.")
+
+# error message
+
+else:
+    print ("ERROR OCCOURED")
