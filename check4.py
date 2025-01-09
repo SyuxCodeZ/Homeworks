2741d7e327a70e47c875c6799a06e852477c20f0
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-09 19:28:10 +0600

    uhagkljuharjhkagha;jkaghk;ujha;jghaahgghhjlgrg;ah;gh;arj;hagrgjhraghhgaaghhgrahh;jargs;jhahr;jagh;jagh;j

---
+++ check4.py
@@ -0,0 +1,11 @@
+# Ask for the user's age
+age = float(input("Enter your age: "))
+
+# Check if the age is between 10 and 20
+if age >= 10:
+    if age <= 20:
+        print("Your age is between 10 and 20.")
+    else:
+        print("Your age is not between 10 and 20.")
+else:
+    print("Your age is not between 10 and 20.")
