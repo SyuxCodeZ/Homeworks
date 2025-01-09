a9ee379800bfb31c490518fe5bb6e0a66590b34e
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-09 19:21:53 +0600

    asdfgjhdfsgjhkfagjhkafhjkafhjkgajgfsjhagfjkgafjgafjhkagfaf;hjkjhkjhkafgfhjhkagjhkajhakjhkagjhkagajkhagjhkajhkhjakjhkhjkagahagjakahjkahjkhajhjahjkahjakhjkhjkhjkasghhjkghjkhjaghjkjagjaghjakjkahjkahjka;hjakjkahjkagjk;ajkgjkaghjkghjk;ajkaghjkakakajhaghjkaghjkaghjkagjagaghjkahjagfkhkjahjkhjkhjgghhjghjghgghghghghghghghgghghhggghghghghhghgkgjgkgklgkagaflkkggfgfgkjfggjkjhkgakhjhkagjhkaaghkgakjgakjgajhkajgkgjkhhlkjlhkhjag

---
+++ store.py
@@ -0,0 +1,11 @@
+bday1 = "21/3/2003"
+bday2 = "21/3/2004"
+bday3 = "21/3/2005"
+bday4 = "21/3/2006"
+bday5 = "21/3/2007"
+
+print (f"Jack's Birthday is {bday1}")
+print (f"Jach's Birthday is {bday2}")
+print (f"Zack's Birthday is {bday3}")
+print (f"Zach's Birthday is {bday4}")
+print (f"Ceaser's Birthday is {bday5}")
