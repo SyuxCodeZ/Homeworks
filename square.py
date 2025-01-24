d0bef8edb61f079743b426252083cfd9f8792256
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-24 20:54:18 +0600

    srfg

---
+++ square.py
@@ -0,0 +1,75 @@
+# importing the module
+
+import turtle
+
+# setting up the pen and the screen
+
+turtle.Screen().bgcolor("beige")
+turtle.Screen().setup(450,500)
+
+# making the pen
+
+square = turtle.Turtle()
+
+# drawing the shape
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+
+square.penup()
+square.forward(100)
+
+square.pendown()
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+
+square.penup()
+square.forward(100)
+
+square.pendown()
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+
+square.penup()
+square.forward(100)
+
+square.pendown()
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+square.right(90)
+
+square.forward(100)
+
+turtle.done()
